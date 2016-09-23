93366683162c6c16b414f7ef61c23c84c6411395
Nikson <razersph3x@gmail.com>
2016-09-23 14:09:47 +0400

    Added entry point

---
+++ Application.java
@@ -0,0 +1,10 @@
+package PO43.PARFENOV.WDAD;
+
+/**
+ * Created by Macintosh on 23.09.2016.
+ */
+public class Application {
+    public static void main(String[] args){
+        System.out.println("I'm, Nikita Parfenov, and I'm not a monkey");
+    }
+}
